b1dc3c1d5b10c82697c75e9b952fc737c9772551
Regis Santos <rg3915@pop-os.localdomain>
2022-07-11 12:37:35 -0300

    Adiciona icones

--- frontend/src/_nav.js
+++ frontend/src/_nav.js
@@ -4,279 +4,367 @@ export default [
     name: 'Dashboard',
     to: '/dashboard',
     icon: 'cil-speedometer',
-    badge: {
-      color: 'primary',
-      text: 'NEW',
-    },
   },
   {
     component: 'CNavTitle',
-    name: 'Theme',
+    name: 'crm',
   },
   {
     component: 'CNavItem',
-    name: 'Colors',
+    name: 'Funcionários',
     to: '/theme/colors',
-    icon: 'cil-drop',
+    icon: 'cil-user',
   },
   {
     component: 'CNavItem',
-    name: 'Typography',
+    name: 'Clientes',
     to: '/theme/typography',
-    icon: 'cil-pencil',
+    icon: 'cil-people',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Contatos',
+    to: '/theme/typography',
+    icon: 'cil-people',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Fornecedores',
+    to: '/theme/typography',
+    icon: 'cil-cursor',
   },
   {
     component: 'CNavTitle',
-    name: 'Components',
-  },
-  {
-    component: 'CNavGroup',
-    name: 'Base',
-    to: '/base',
-    icon: 'cil-puzzle',
-    items: [
-      {
-        component: 'CNavItem',
-        name: 'Accordion',
-        to: '/base/accordion',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Breadcrumbs',
-        to: '/base/breadcrumbs',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Cards',
-        to: '/base/cards',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Carousels',
-        to: '/base/carousels',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Collapses',
-        to: '/base/collapses',
-      },
-      {
-        component: 'CNavItem',
-        name: 'List Groups',
-        to: '/base/list-groups',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Navs & Tabs',
-        to: '/base/navs',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Paginations',
-        to: '/base/paginations',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Placeholders',
-        to: '/base/placeholders',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Popovers',
-        to: '/base/popovers',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Progress',
-        to: '/base/progress',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Spinners',
-        to: '/base/spinners',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Tables',
-        to: '/base/tables',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Tooltips',
-        to: '/base/tooltips',
-      },
-    ],
-  },
-  {
-    component: 'CNavGroup',
-    name: 'Buttons',
-    to: '/buttons',
-    icon: 'cil-cursor',
-    items: [
-      {
-        component: 'CNavItem',
-        name: 'Buttons',
-        to: '/buttons/standard-buttons',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Button Groups',
-        to: '/buttons/button-groups',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Dropdowns',
-        to: '/buttons/dropdowns',
-      },
-    ],
-  },
-  {
-    component: 'CNavGroup',
-    name: 'Forms',
-    to: '/forms',
-    icon: 'cil-notes',
-    items: [
-      {
-        component: 'CNavItem',
-        name: 'Form Control',
-        to: '/forms/form-control',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Select',
-        to: '/forms/select',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Checks & Radios',
-        to: '/forms/checks-radios',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Range',
-        to: '/forms/range',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Input Group',
-        to: '/forms/input-group',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Floating Labels',
-        to: '/forms/floating-labels',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Layout',
-        to: '/forms/layout',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Validation',
-        to: '/forms/validation',
-      },
-    ],
+    name: 'Financeiro',
   },
   {
     component: 'CNavItem',
-    name: 'Charts',
-    to: '/charts',
-    icon: 'cil-chart-pie',
+    name: 'Financeiro',
+    to: '/theme/colors',
+    icon: 'cil-dollar',
   },
   {
-    component: 'CNavGroup',
-    name: 'Icons',
-    to: '/icons',
-    icon: 'cil-star',
-    items: [
-      {
-        component: 'CNavItem',
-        name: 'CoreUI Icons',
-        to: '/icons/coreui-icons',
-        badge: {
-          color: 'info',
-          text: 'NEW',
-        },
-      },
-      {
-        component: 'CNavItem',
-        name: 'Brands',
-        to: '/icons/brands',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Flags',
-        to: '/icons/flags',
-      },
-    ],
-  },
-  {
-    component: 'CNavGroup',
-    name: 'Notifications',
-    to: '/notifications',
-    icon: 'cil-bell',
-    items: [
-      {
-        component: 'CNavItem',
-        name: 'Alerts',
-        to: '/notifications/alerts',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Badges',
-        to: '/notifications/badges',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Modals',
-        to: '/notifications/modals',
-      },
-    ],
+    component: 'CNavTitle',
+    name: 'Notas de Reembolso',
   },
   {
     component: 'CNavItem',
-    name: 'Widgets',
-    to: '/widgets',
-    icon: 'cil-calculator',
+    name: 'NR dos Clientes',
+    to: '/theme/colors',
+    icon: 'cil-book',
     badge: {
-      color: 'primary',
-      text: 'NEW',
-      shape: 'pill',
+      color: 'danger',
+      text: '10',
+    },
+  },
+  {
+    component: 'CNavItem',
+    name: 'NR Funcionários',
+    to: '/theme/colors',
+    icon: 'cil-book',
+    badge: {
+      color: 'danger',
+      text: '10',
+    },
+  },
+  {
+    component: 'CNavItem',
+    name: 'ND Fonte Pagadora',
+    to: '/theme/colors',
+    icon: 'cil-book',
+    badge: {
+      color: 'danger',
+      text: '10',
     },
   },
   {
     component: 'CNavTitle',
-    name: 'Extras',
+    name: 'Contrato',
   },
   {
-    component: 'CNavGroup',
-    name: 'Pages',
-    to: '/pages',
-    icon: 'cil-star',
-    items: [
-      {
-        component: 'CNavItem',
-        name: 'Login',
-        to: '/pages/login',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Register',
-        to: '/pages/register',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Error 404',
-        to: '/pages/404',
-      },
-      {
-        component: 'CNavItem',
-        name: 'Error 500',
-        to: '/pages/500',
-      },
-    ],
+    component: 'CNavItem',
+    name: 'Contratos',
+    to: '/theme/colors',
+    icon: 'cil-layers',
   },
+  {
+    component: 'CNavTitle',
+    name: 'TimeSheet',
+  },
+  {
+    component: 'CNavItem',
+    name: 'TimeSheet',
+    to: '/theme/colors',
+    icon: 'cil-clock',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Relatórios',
+    to: '/theme/typography',
+    icon: 'cil-list',
+  },
+  {
+    component: 'CNavTitle',
+    name: 'Pastas',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Pastas',
+    to: '/theme/colors',
+    icon: 'cil-folder',
+  },
+  // {
+  //   component: 'CNavTitle',
+  //   name: 'Components',
+  // },
+  // {
+  //   component: 'CNavGroup',
+  //   name: 'Base',
+  //   to: '/base',
+  //   icon: 'cil-puzzle',
+  //   items: [
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Accordion',
+  //       to: '/base/accordion',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Breadcrumbs',
+  //       to: '/base/breadcrumbs',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Cards',
+  //       to: '/base/cards',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Carousels',
+  //       to: '/base/carousels',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Collapses',
+  //       to: '/base/collapses',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'List Groups',
+  //       to: '/base/list-groups',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Navs & Tabs',
+  //       to: '/base/navs',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Paginations',
+  //       to: '/base/paginations',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Placeholders',
+  //       to: '/base/placeholders',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Popovers',
+  //       to: '/base/popovers',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Progress',
+  //       to: '/base/progress',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Spinners',
+  //       to: '/base/spinners',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Tables',
+  //       to: '/base/tables',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Tooltips',
+  //       to: '/base/tooltips',
+  //     },
+  //   ],
+  // },
+  // {
+  //   component: 'CNavGroup',
+  //   name: 'Buttons',
+  //   to: '/buttons',
+  //   icon: 'cil-cursor',
+  //   items: [
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Buttons',
+  //       to: '/buttons/standard-buttons',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Button Groups',
+  //       to: '/buttons/button-groups',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Dropdowns',
+  //       to: '/buttons/dropdowns',
+  //     },
+  //   ],
+  // },
+  // {
+  //   component: 'CNavGroup',
+  //   name: 'Forms',
+  //   to: '/forms',
+  //   icon: 'cil-notes',
+  //   items: [
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Form Control',
+  //       to: '/forms/form-control',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Select',
+  //       to: '/forms/select',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Checks & Radios',
+  //       to: '/forms/checks-radios',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Range',
+  //       to: '/forms/range',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Input Group',
+  //       to: '/forms/input-group',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Floating Labels',
+  //       to: '/forms/floating-labels',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Layout',
+  //       to: '/forms/layout',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Validation',
+  //       to: '/forms/validation',
+  //     },
+  //   ],
+  // },
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Charts',
+  //   to: '/charts',
+  //   icon: 'cil-chart-pie',
+  // },
+  // {
+  //   component: 'CNavGroup',
+  //   name: 'Icons',
+  //   to: '/icons',
+  //   icon: 'cil-star',
+  //   items: [
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'CoreUI Icons',
+  //       to: '/icons/coreui-icons',
+  //       badge: {
+  //         color: 'info',
+  //         text: 'NEW',
+  //       },
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Brands',
+  //       to: '/icons/brands',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Flags',
+  //       to: '/icons/flags',
+  //     },
+  //   ],
+  // },
+  // {
+  //   component: 'CNavGroup',
+  //   name: 'Notifications',
+  //   to: '/notifications',
+  //   icon: 'cil-bell',
+  //   items: [
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Alerts',
+  //       to: '/notifications/alerts',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Badges',
+  //       to: '/notifications/badges',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Modals',
+  //       to: '/notifications/modals',
+  //     },
+  //   ],
+  // },
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Widgets',
+  //   to: '/widgets',
+  //   icon: 'cil-calculator',
+  //   badge: {
+  //     color: 'primary',
+  //     text: 'NEW',
+  //     shape: 'pill',
+  //   },
+  // },
+  // {
+  //   component: 'CNavTitle',
+  //   name: 'Extras',
+  // },
+  // {
+  //   component: 'CNavGroup',
+  //   name: 'Pages',
+  //   to: '/pages',
+  //   icon: 'cil-star',
+  //   items: [
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Login',
+  //       to: '/pages/login',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Register',
+  //       to: '/pages/register',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Error 404',
+  //       to: '/pages/404',
+  //     },
+  //     {
+  //       component: 'CNavItem',
+  //       name: 'Error 500',
+  //       to: '/pages/500',
+  //     },
+  //   ],
+  // },
 
   // {
   //   component: 'CNavItem',
