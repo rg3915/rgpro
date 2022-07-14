2146ba4f4086bcb3c96207cdd4b35b3dbbc9adc4
rg3915 <regis42santos@gmail.com>
2022-07-14 04:46:26 -0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ npm run build
 cd dist
 
 git init
+git remote add origin git@github.com:rg3915/rgpro.git
+git checkout -b main
 git add -A
 git commit -m 'New Deployment'
 git push -f git@github.com:rg3915/rgpro.git main:gh-pages
